fe3b79b463e59959270342a02afd65b10bc19d55
bgonzax <bgonzales20001@gmail.com>
2015-02-27 17:09:09 -0800

    vehicle admin

--- static/js/app/controllers.js
+++ static/js/app/controllers.js
@@ -1,8 +1,8 @@
 
 
-var phonecatApp = angular.module('phonecatApp', []);
+var jaegerApp = angular.module('jaegerApp', []);
 
-phonecatApp.controller('PhoneListCtrl', function ($scope) {
+jaegerApp.controller('PhoneListCtrl', function ($scope) {
   $scope.phones = [
     {'name': 'Nexus S',
      'snippet': 'Fast just got faster with Nexus S.'},
@@ -12,3 +12,45 @@ phonecatApp.controller('PhoneListCtrl', function ($scope) {
      'snippet': 'The Next, Next Generation tablet.'}
   ];
 });
+
+jaegerApp.controller('itemController', function ($scope) {
+
+	$scope.upload = function() {alert("aaaa");};
+	$scope.delete = function() {alert("delete item?");};
+	$scope.slideItemMessage = "shdfjhsdfjk hsjkfh sdjkfhsdjkfh jksdfh jksdhfjk sdhf jksdhf jksdhf jksdhf jksdh fjksdhfjk fasdfsdf";
+
+});
+
+
+jaegerApp.directive('contenteditable', function () {
+      return {
+          restrict: 'A', // only activate on element attribute
+          require: '?ngModel', // get a hold of NgModelController
+          link: function (scope, element, attrs, ngModel) {
+              if (!ngModel) return; // do nothing if no ng-model
+
+              // Specify how UI should be updated
+              ngModel.$render = function () {
+                  element.html(ngModel.$viewValue || '');
+              };
+
+              // Listen for change events to enable binding
+              element.on('blur keyup change', function () {
+                  scope.$apply(readViewText);
+              });
+
+              // No need to initialize, AngularJS will initialize the text based on ng-model attribute
+
+              // Write data to the model
+              function readViewText() {
+                  var html = element.html();
+                  // When we clear the content editable the browser leaves a <br> behind
+                  // If strip-br attribute is provided then we strip this out
+                  if (attrs.stripBr && html == '<br>') {
+                      html = '';
+                  }
+                  ngModel.$setViewValue(html);
+              }
+          }
+      };
+  });
